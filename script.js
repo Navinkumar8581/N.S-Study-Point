12399aee77c9f31e7410ca960e677c17c56c062a
Navinkumar8581 <navinj522@gmail.com>
2025-03-18 13:10:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("registrationForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Registration successful!");
+});
+
+document.getElementById("inquiryForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Inquiry submitted successfully!");
+});
